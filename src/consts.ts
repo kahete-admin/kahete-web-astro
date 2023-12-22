4c3793b6017b131ab5db75be7f1e93b0cbb5ff0a
Luis Siliezar <luis.siliezar@tkiero.app>
2023-12-22 08:48:22 -0600

    chore: changed constant

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Kahete';
-export const SITE_DESCRIPTION = 'Presentation is key';
+export const SITE_DESCRIPTION = 'Creating the best presentation experience';
