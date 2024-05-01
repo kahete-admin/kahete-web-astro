91afa3e4148f475cc4bc04f7e7efc81b835cc757
lmajano07 <lmajano0701@gmail.com>
2024-04-30 22:38:20 -0600

    chore: add adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel/serverless";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
+  adapter: vercel(),
   site: "https://kahete.com",
   integrations: [mdx(), sitemap(), tailwind()],
 });
