a5944d0ae75dab38e329690e70778f9c017e9a1d
lmajano07 <lmajano0701@gmail.com>
2024-05-27 22:30:13 -0600

    chore: load data from api

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,15 +1,18 @@
 export function orderDataByUsername(incomingData) {
-    return incomingData
-        .slice()
-        .sort((a, b) => a.username.localeCompare(b.username));
+  return incomingData
+    .slice()
+    .sort((a, b) =>
+      a.basic_user_profile.name.localeCompare(b.basic_user_profile.name)
+    );
 }
 
 // ADD NULL TO SECOND PARAM IF ALL DEPARTMENTS ARE SELECTED
 export function sectionTitleFilter(incomingData, filterName) {
-    if (filterName === null) {
-        return orderDataByUsername(incomingData);
-    }
+  if (filterName === null) {
+    return orderDataByUsername(incomingData);
+  }
 
-    return orderDataByUsername(incomingData)
-        .filter((entry) => entry.title === filterName);
+  return orderDataByUsername(incomingData).filter(
+    (entry) => entry.basic_user_profile.title === filterName
+  );
 }
