af22d1da131a91668b92b8b807dd69b30eee23a2
kahete-admin <163953505+kahete-admin@users.noreply.github.com>
2024-05-29 20:59:30 -0600

    Feature: Load data from API (#3)
    * chore: load data from api
    
    * fix: renamed function
    
    * chore: data name changes
    
    ---------
    
    Co-authored-by: lmajano07 <lmajano0701@gmail.com>

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,15 +1,26 @@
 export function orderDataByUsername(incomingData) {
-    return incomingData
-        .slice()
-        .sort((a, b) => a.username.localeCompare(b.username));
+  return incomingData
+    .slice()
+    .sort((a, b) =>
+      a.basic_user_profile.full_name.localeCompare(
+        b.basic_user_profile.full_name
+      )
+    );
+}
+
+export function orderDataByUsernameLaConfe(incomingData) {
+  return incomingData
+    .slice()
+    .sort((a, b) => a.username.localeCompare(b.username));
 }
 
 // ADD NULL TO SECOND PARAM IF ALL DEPARTMENTS ARE SELECTED
 export function sectionTitleFilter(incomingData, filterName) {
-    if (filterName === null) {
-        return orderDataByUsername(incomingData);
-    }
+  if (filterName === null) {
+    return orderDataByUsername(incomingData);
+  }
 
-    return orderDataByUsername(incomingData)
-        .filter((entry) => entry.title === filterName);
+  return orderDataByUsername(incomingData).filter(
+    (entry) => entry.basic_user_profile.title === filterName
+  );
 }
