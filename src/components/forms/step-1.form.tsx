2b94db8be4665e7113c93284ac468550d3429ae8
Luis Siliezar <lsiliezarjr@gmail.com>
2024-12-02 09:01:39 -0600

    chore: added step 1 dummy

--- src/components/forms/step-1.form.tsx
+++ src/components/forms/step-1.form.tsx
@@ -1,75 +1,74 @@
-// import { Input } from "@/components/ui/input";
-// import { AtSign, CircleCheck, Mail } from "lucide-react";
-// import { PhoneInput } from "@/components/ui/phone-input";
-// import { useState } from "react";
-// import { FlagComponent, CountrySelect } from "@/components/ui/country-select";
-// import * as RPNInput from "react-phone-number-input";
+"use client"
+import { Button } from "../ui/button";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { CircleCheck } from "lucide-react";
 
-// export const Step1Form = () => {
-//     const [value, setValue] = useState("");
-//     return (
-//         <div className="items-center justify-center w-full pt-12 px-4">
-//             {/* Regular */}
-//             <Input placeholder="Email" type="email" />
-//             {/* With icon */}
-//             <div className="pt-4">
-//                 <div className="relative">
-//                     <Input id="input-09" className="peer ps-9" placeholder="Email" type="email" />
-//                     <div className="pointer-events-none absolute inset-y-0 start-0 flex items-center justify-center ps-3 text-muted-foreground/80 peer-disabled:opacity-50">
-//                         <AtSign size={16} strokeWidth={2} aria-hidden="true" />
-//                     </div>
-//                 </div>
-//             </div>
+const formSchema = z.object({
+    username:
+        z.string()
+            .min(2, {
+                message: "Username must be at least 2 characters.",
+            })
+            .refine((value) => /^[a-zA-Z]+[-'s]?[a-zA-Z ]+$/.test(value ?? ""), 'Name should contain only alphabets'),
+});
 
-//             {/* With label */}
-//             <div className="pt-4">
-//                 <div className="relative">
-//                     <Input className="peer ps-24" placeholder="your-name" type="text" maxLength={12} />
-//                     <span className="pointer-events-none absolute inset-y-0 start-0 flex items-center justify-center ps-3 text-sm text-white peer-disabled:opacity-50">
-//                         kahete.com/
-//                     </span>
-//                 </div>
-//             </div>
 
-//             {/* With Icon at the end  */}
-//             <div className="pt-4">
-//                 <div className="relative">
-//                     <Input id="input-10" className="peer pe-9" placeholder="Email" type="email" />
-//                     <div className="pointer-events-none absolute inset-y-0 end-0 flex items-center justify-center pe-3 text-muted-foreground/80 peer-disabled:opacity-50">
-//                         <Mail size={16} strokeWidth={2} aria-hidden="true" />
-//                     </div>
-//                 </div>
-//             </div>
 
-//             {/* With Icon and label */}
-//             <div className="pt-4">
-//                 <div className="relative">
-//                     <span className="pointer-events-none absolute inset-y-0 start-0 flex items-center justify-center ps-3 text-sm text-white peer-disabled:opacity-50">
-//                         kahete.com/
-//                     </span>
-//                     <Input className="peer ps-24" placeholder="your-name" type="text" maxLength={12} />
-//                     {/* Check icon */}
-//                     <div className="pointer-events-none absolute inset-y-0 end-0 flex items-center justify-center pe-3 text-muted-foreground/80 peer-disabled:opacity-50">
-//                         <CircleCheck color="green" size={16} strokeWidth={2} aria-hidden="true" />
-//                     </div>
-//                 </div>
-//             </div>
+export const Step1Form = () => {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            username: "",
+        },
+    })
 
-//             {/* PHONE */}
-//             <div className="pt-4" dir="ltr">
-//                 <RPNInput.default
-//                     className="flex rounded-lg shadow-sm shadow-black/5 bg-[#191E21]"
-//                     international
-//                     flagComponent={FlagComponent}
-//                     countrySelectComponent={CountrySelect}
-//                     inputComponent={PhoneInput}
-//                     id="input-46"
-//                     placeholder="Enter phone number"
-//                     value={value}
-//                     onChange={(newValue) => setValue(newValue ?? "")}
-//                 />
-//             </div>
+    // 2. Define a submit handler.
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values)
+    }
 
-//         </div >
-//     );
-// };
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="px-4">
+                <FormField
+                    control={form.control}
+                    name="username"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormControl>
+                                <div className="pt-4">
+                                    <div className="relative">
+                                        <span className="pointer-events-none absolute inset-y-0 start-0 flex items-center justify-center ps-3 text-sm text-white peer-disabled:opacity-50">
+                                            kahete.com/
+                                        </span>
+                                        <Input className="peer ps-24 text-sm" placeholder="your-name" type="text" maxLength={12} {...field} />
+                                        {/* Check icon */}
+                                        {/* <div className="pointer-events-none absolute inset-y-0 end-0 flex items-center justify-center pe-3 text-muted-foreground/80 peer-disabled:opacity-50">
+                                            <CircleCheck color="green" size={16} strokeWidth={2} aria-hidden="true" />
+                                        </div> */}
+                                    </div>
+                                </div>
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <div className="absolute bottom-5 right-0 px-4 w-full">
+                    <Button type="submit" size={'full'}>Submit</Button>
+                </div>
+            </form>
+        </Form>
+    );
+};
